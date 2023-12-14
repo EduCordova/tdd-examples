6998ae6fbe4b3d87e709c21914afc496fc90f323
Eduardo <kde1690@gmail.com>
2023-12-13 20:11:21 -0500

    fix: test for size

--- test/can-reconfigure.test.js
+++ test/can-reconfigure.test.js
@@ -4,6 +4,9 @@ import { describe, it, expect }  from 'vitest';
 const canReconfigure = (from, to) => {
     if (typeof from !== 'string') throw new Error('from is not a string')
     if (typeof to !== 'string') throw new Error('second param is required')
+    if (to.length !== from.length) return false
+
+    return true
 }
 
 describe('canReconfigure', () => {
@@ -17,5 +20,13 @@ describe('canReconfigure', () => {
 
     it('shoul validate second páram', () => {
         expect(() => canReconfigure('a')).toThrow()
+    })
+
+    it('should return a boolean', () => {
+        expect(canReconfigure('a','b')).toBeTypeOf('boolean')
+    })
+
+    it('should return a false if strings provided have different length', () => {
+        expect(canReconfigure('axa','be')).toBe(false)
     })
 })
